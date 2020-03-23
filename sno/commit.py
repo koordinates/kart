16d2777b9963153608c2231a1c1ffb045bd0522c
Andrew Olsen <andrew.olsen@koordinates.com>
2020-03-24 09:16:10 +1300

    Add json output for sno commit.

--- sno/commit.py
+++ sno/commit.py
@@ -1,7 +1,11 @@
+import contextlib
+import logging
+import json
 import os
 import re
 import shlex
 import subprocess
+import sys
 from pathlib import Path
 
 import click
@@ -13,6 +17,29 @@ from .status import get_branch_status_message, get_diff_status_message
 from .working_copy import WorkingCopy
 from .structure import RepositoryStructure
 from .cli_util import MutexOption
+
+
+@contextlib.contextmanager
+def _info_to_stdout(logger):
+    old_effective_level = logger.getEffectiveLevel()
+    old_level = logger.level
+
+    def log_at_root(r):
+        return r.name != logger.name or r.levelno >= old_effective_level
+
+    def log_to_stdout(r):
+        return r.name == logger.name and r.levelno == logging.INFO
+
+    handler = logging.StreamHandler(sys.stdout)
+    handler.addFilter(log_to_stdout)
+
+    logger.root.handlers[0].addFilter(log_at_root)
+    logger.setLevel(logging.INFO)
+    logger.addHandler(handler)
+    yield logger
+    logger.removeHandler(handler)
+    logger.setLevel(old_level)
+    logger.root.handlers[0].removeFilter(log_at_root)
 
 
 @click.command()
@@ -44,7 +71,24 @@ from .cli_util import MutexOption
         "such a commit. This option bypasses the safety"
     ),
 )
-def commit(ctx, message, message_file, allow_empty):
+@click.option(
+    "--text",
+    "is_output_json",
+    flag_value=False,
+    default=True,
+    help="Commit result shown in text format",
+    cls=MutexOption,
+    exclusive_with=["json"],
+)
+@click.option(
+    "--json",
+    "is_output_json",
+    flag_value=True,
+    help="Commit result shown in JSON format",
+    cls=MutexOption,
+    exclusive_with=["text"],
+)
+def commit(ctx, message, message_file, allow_empty, is_output_json):
     """ Record changes to the repository """
     repo = ctx.obj.repo
 
@@ -80,7 +124,23 @@ def commit(ctx, message, message_file, allow_empty):
     if not commit_msg:
         raise click.Abort()
 
-    new_commit = rs.commit(wcdiff, commit_msg, allow_empty=allow_empty)
+    log_tweak = contextlib.nullcontext()
+    if not is_output_json:
+        log_tweak = _info_to_stdout(logging.getLogger("sno.structure"))
+
+    with log_tweak:
+        new_commit = rs.commit(wcdiff, commit_msg, allow_empty=allow_empty)
+
+    if is_output_json:
+        json.dump(
+            {
+                "sno.commit/v1": {
+                    "commit": str(new_commit),
+                    "tree": str(repo.head.peel(pygit2.Tree).id)
+                }
+            },
+            sys.stdout,
+            indent=2)
 
 
 def get_commit_message(repo, wc_changes):
