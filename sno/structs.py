d79e05a8dd75b5e20891e4f53e823a5dd2836869
Andrew Olsen <andrew.olsen@koordinates.com>
2020-05-05 13:42:21 +1200

    New class ConflictIndex - like pygit2.Index with serialisable conflicts

--- sno/structs.py
+++ sno/structs.py
@@ -72,9 +72,38 @@ class CommitWithReference:
         return CommitWithReference(repo.resolve_refish(refish))
 
 
-# When merging two commits, we have three versions of lots of things - these three versions
-# are the common ancestor, and the two versions to be merged, "ours" and "theirs".
-AncestorOursTheirs = namedtuple("AncestorOursTheirs", ("ancestor", "ours", "theirs"))
+# pygit2 always has this order - we use it too for consistency,
+# and so we can meaningfully zip() our tuples with theirs
+_ANCESTOR_OURS_THEIRS_ORDER = ("ancestor", "ours", "theirs")
 
-AncestorOursTheirs.names = AncestorOursTheirs._fields
-AncestorOursTheirs.chars = tuple(n[0] for n in AncestorOursTheirs.names)
+
+class AncestorOursTheirs(namedtuple("AncestorOursTheirs", _ANCESTOR_OURS_THEIRS_ORDER)):
+    """
+    When merging two commits, we can end up with three versions of lots of things -
+    commits, repository-structures, datasets, features, primary keys.
+    The 3 versions  are the common ancestor, and 2 versions to be merged, "ours" and "theirs".
+    Like pygit2, we keep the 3 versions always in the same order - ancestor, ours, theirs.
+    """
+
+    NAMES = _ANCESTOR_OURS_THEIRS_ORDER
+    CHARS = tuple(n[0] for n in NAMES)
+
+    @staticmethod
+    def partial(*, ancestor=None, ours=None, theirs=None):
+        """Supply some or all keyword arguments: ancestor, ours, theirs"""
+        return AncestorOursTheirs(ancestor, ours, theirs)
+
+    def __or__(self, other):
+        # We don't allow any field to be set twice
+        assert not self.ancestor or not other.ancestor
+        assert not self.ours or not other.ours
+        assert not self.theirs or not other.theirs
+        result = AncestorOursTheirs(
+            ancestor=self.ancestor or other.ancestor,
+            ours=self.ours or other.ours,
+            theirs=self.theirs or other.theirs,
+        )
+        return result
+
+
+AncestorOursTheirs.EMPTY = AncestorOursTheirs(None, None, None)
