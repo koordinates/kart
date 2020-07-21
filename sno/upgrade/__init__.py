c667f7d664611d8d9876e301fe61337fd4832461
Craig de Stigter <craig@destigter.nz>
2020-07-21 16:37:19 +1200

    Simplify upgrade command
    The magic imports here were causing PyInstaller to not include the
    subcommand modules on linux and windows
    
    Fixes #172

--- sno/upgrade/__init__.py
+++ sno/upgrade/__init__.py
@@ -1,27 +1,13 @@
-import importlib.util
-from pathlib import Path
-
 import click
 
-
-class UpgradeCommand(click.MultiCommand):
-    plugin_path = Path(__file__).parent
-
-    def list_commands(self, ctx):
-        rv = []
-        for filename in self.plugin_path.glob("upgrade_*.py"):
-            rv.append(filename.stem[8:].replace("_", "-"))
-        rv.sort()
-        return rv
-
-    def get_command(self, ctx, name):
-        module_name = f"sno.upgrade.upgrade_{name.replace('-', '_')}"
-
-        module = importlib.import_module(module_name)
-        return module.upgrade
+from . import upgrade_00_02, upgrade_02_05  # noqa
 
 
-@click.command(cls=UpgradeCommand)
+@click.group()
 def upgrade():
     """ Upgrade repositories between versions of Sno """
     pass
+
+
+upgrade.add_command(upgrade_00_02.upgrade, name='00-02')
+upgrade.add_command(upgrade_02_05.upgrade, name='02-05')
