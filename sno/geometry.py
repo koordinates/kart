329349e7bdf18932a10f1294a20fc1fd8aee4e27
Andrew Olsen <andrew.olsen@koordinates.com>
2020-09-29 11:58:35 +1300

    Manually merge in postgis.py from postgis-working-copy1 branch
    Some changes needed to geometry.py to make it 'work'. Not used so far.
    Keeping postgis.py just as in postgis-working-copy1 in this commit -
    - lots to change in further commits, including moving common code to
    common superclass, base.WorkingCopy

--- sno/geometry.py
+++ sno/geometry.py
@@ -222,6 +222,28 @@ def gpkg_geom_to_hex_wkb(gpkg_geom):
         return binascii.hexlify(wkb).decode("ascii").upper()
 
 
+def parse_gpkg_geom(gpkg_geom):
+    if not isinstance(gpkg_geom, bytes):
+        raise TypeError("Expected bytes")
+
+    if gpkg_geom[0:2] != b"GP":  # 0x4750
+        raise ValueError("Expected GeoPackage Binary Geometry")
+    (version, flags) = struct.unpack_from("BB", gpkg_geom, 2)
+    if version != 0:
+        raise NotImplementedError("Expected GeoPackage v1 geometry, got %d", version)
+
+    is_le = (flags & 0b0000001) != 0  # Endian-ness
+
+    if flags & (0b00100000):  # GeoPackageBinary type
+        raise NotImplementedError("ExtendedGeoPackageBinary")
+
+    wkb_offset = 8 + gpkg_envelope_size(flags)
+
+    crs_id = struct.unpack_from(f"{_bo(is_le)}i", gpkg_geom, 4)[0]
+
+    return wkb_offset, is_le, crs_id
+
+
 def gpkg_geom_to_ogr(gpkg_geom, parse_crs=False):
     """
     Parse GeoPackage geometry values to an OGR Geometry object
@@ -230,24 +252,15 @@ def gpkg_geom_to_ogr(gpkg_geom, parse_crs=False):
     if gpkg_geom is None:
         return None
 
-    flags = _validate_gpkg_geom(gpkg_geom)
-    is_le = (flags & _GPKG_LE_BIT) != 0  # Endian-ness
+    wkb_offset, is_le, crs_id = parse_gpkg_geom(gpkg_geom)
 
-    wkb_offset = 8 + gpkg_envelope_size(flags)
-    wkb = gpkg_geom[wkb_offset:]
+    geom = ogr.CreateGeometryFromWkb(gpkg_geom[wkb_offset:])
+    assert geom is not None
 
-    # note: the GPKG spec represents 'POINT EMPTY' as 'POINT(nan nan)' (in WKB form)
-    # However, OGR loads the WKB for POINT(nan nan) as an empty geometry.
-    # It has the WKB of `POINT(nan nan)` but the WKT of `POINT EMPTY`.
-    # We just leave it as-is.
-    geom = ogr.CreateGeometryFromWkb(wkb)
-
-    if parse_crs:
-        crs_id = struct.unpack_from(f"{'<' if is_le else '>'}i", gpkg_geom, 4)[0]
-        if crs_id > 0:
-            spatial_ref = osr.SpatialReference()
-            spatial_ref.ImportFromEPSG(crs_id)
-            geom.AssignSpatialReference(spatial_ref)
+    if parse_crs and crs_id > 0:
+        spatial_ref = osr.SpatialReference()
+        spatial_ref.ImportFromEPSG(crs_id)
+        geom.AssignSpatialReference(spatial_ref)
 
     return geom
 
@@ -354,6 +367,105 @@ def geojson_to_gpkg_geom(geojson, **kwargs):
 
     ogr_geom = ogr.CreateGeometryFromJson(json_ogr)
     return ogr_to_gpkg_geom(ogr_geom, **kwargs)
+
+
+def _bo(is_le):
+    """Returns the byte order instruction for struct.pack"""
+    return "<" if is_le else ">"
+
+
+def geom_to_ewkb(gpkg_geom):
+    """
+    Parse GeoPackage geometry values to a PostGIS EWKB value
+    http://www.geopackage.org/spec/#gpb_format
+    """
+    if gpkg_geom is None:
+        return None
+
+    wkb_offset, is_le, srid = parse_gpkg_geom(gpkg_geom)
+
+    wkb_is_le = struct.unpack_from("B", gpkg_geom, wkb_offset)[0]
+    bo = _bo(wkb_is_le)
+
+    wkb_type = struct.unpack_from(f"{bo}I", gpkg_geom, wkb_offset + 1)[0]
+    wkb_geom_type = (wkb_type & 0xFFFF) % 1000
+    iso_zm = (wkb_type & 0xFFFF) // 1000
+    has_z = iso_zm in (1, 3)
+    has_m = iso_zm in (2, 3)
+
+    ewkb_geom_type = wkb_geom_type
+    ewkb_geom_type |= 0x80000000 * has_z
+    ewkb_geom_type |= 0x40000000 * has_m
+    ewkb_geom_type |= 0x20000000 * (srid > 0)
+
+    ewkb = struct.pack(f"{bo}BI", int(wkb_is_le), ewkb_geom_type)
+
+    if srid > 0:
+        ewkb += struct.pack(f"{bo}I", srid)
+
+    ewkb += gpkg_geom[(wkb_offset + 5) :]
+
+    return ewkb
+
+
+def hexewkb_to_geom(hexewkb):
+    """
+    Parse PostGIS Hex EWKB to GeoPackage geometry
+    https://github.com/postgis/postgis/blob/master/doc/ZMSgeoms.txt
+    """
+    if hexewkb is None:
+        return None
+
+    ewkb = bytes.fromhex(hexewkb)
+    is_le = struct.unpack_from("B", ewkb)[0]
+    bo = _bo(is_le)
+
+    ewkb_type = struct.unpack_from(f"{bo}I", ewkb, 1)[0]
+    has_z = bool(ewkb_type & 0x80000000)
+    has_m = bool(ewkb_type & 0x40000000)
+    has_srid = bool(ewkb_type & 0x20000000)
+
+    geom_type = ewkb_type & 0xFFFF
+    wkb_type = geom_type + 1000 * has_z + 2000 * has_m
+
+    data_offset = 5
+    if has_srid:
+        srid = struct.unpack_from(f"{bo}I", ewkb, data_offset)[0]
+        data_offset += 4
+    else:
+        srid = 0
+
+    if wkb_type % 1000 == 1:
+        # detect POINT[ZM] EMPTY
+        px, py = struct.unpack_from(f"{bo}dd", ewkb, data_offset)
+        is_empty = math.isnan(px) and math.isnan(py)
+    else:
+        wkb_num = struct.unpack_from(f"{bo}I", ewkb, data_offset)[
+            0
+        ]  # num(Points|Rings|Polygons|...)
+        is_empty = wkb_num == 0
+
+    flags = 0
+    if is_le:
+        flags |= 1
+    if is_empty:
+        flags |= 0b00010000
+
+    gpkg_geom = (
+        struct.pack(
+            f"{bo}ccBBiBI",
+            b"G",
+            b"P",
+            0,
+            flags,
+            srid,
+            int(is_le),
+            wkb_type,  # version
+        )
+        + ewkb[data_offset:]
+    )
+
+    return gpkg_geom
 
 
 def geom_envelope(gpkg_geom):
