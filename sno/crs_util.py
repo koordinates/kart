dc8b5fc791749c816f18cb50f6ce156d7505bd7a
Andrew Olsen <andrew.olsen@koordinates.com>
2020-10-20 11:13:37 +1300

    Don't rely on SpatialReference for parsing.

--- sno/crs_util.py
+++ sno/crs_util.py
@@ -1,3 +1,5 @@
+import re
+
 from osgeo.osr import SpatialReference
 
 from .cli_util import StringFromFile
@@ -19,37 +21,105 @@ class CoordinateReferenceString(StringFromFile):
             )
 
 
-def get_identifier_str(crs):
+class _WktPatterns:
+    """Regular expressions for recognizing WKT name and authority."""
+
+    WHITESPACE = r"\s*"
+    OPEN_BRACKET = r"[%s]" % re.escape("[(")
+    CLOSE_BRACKET = r"[%s]" % re.escape("])")
+
+    WKT_STR = r'"((?:""|[^"])*)"'
+
+    ROOT_NAME_PATTERN = re.compile(
+        WHITESPACE.join(["^", "[A-Z]*", OPEN_BRACKET, WKT_STR]), re.IGNORECASE
+    )
+
+    ROOT_AUTHORITY_PATTERN = re.compile(
+        WHITESPACE.join(
+            [
+                "AUTHORITY",
+                OPEN_BRACKET,
+                WKT_STR,
+                ",",
+                WKT_STR,
+                CLOSE_BRACKET,
+                CLOSE_BRACKET,
+                "$",
+            ]
+        ),
+        re.IGNORECASE,
+    )
+
+
+def parse_name(crs):
+    if isinstance(crs, str):
+        m = _WktPatterns.ROOT_NAME_PATTERN.search(crs)
+        if m:
+            return m.group(1)
+        else:
+            spatial_ref = SpatialReference(crs)
+    elif isinstance(crs, SpatialReference):
+        spatial_ref = crs
+    else:
+        raise RuntimeError(f"Unrecognised CRS: {crs}")
+    return spatial_ref.GetName()
+
+
+def parse_authority(crs):
+    if isinstance(crs, str):
+        m = _WktPatterns.ROOT_AUTHORITY_PATTERN.search(crs)
+        if m:
+            return m.group(1), m.group(2)
+        spatial_ref = SpatialReference(crs)
+    elif isinstance(crs, SpatialReference):
+        spatial_ref = crs
+    else:
+        raise RuntimeError(f"Unrecognised CRS: {crs}")
+
+    # Use osgeo only as a fallback if the regex failed -
+    # We avoid using it since it is opinionated and sometimes chooses not to return the information it parsed -
+    # if it doesn't think the authority matches the CRS, it might just ignore it.
+    return spatial_ref.GetAuthorityName(None), spatial_ref.GetAuthorityCode(None)
+
+
+def get_identifier_str(crs, authority=None):
     """
     Given a CRS, generate a stable, unique identifier for it of type 'str'. Eg: "EPSG:2193"
     """
-    if isinstance(crs, str):
-        crs = SpatialReference(crs)
-    if not isinstance(crs, SpatialReference):
-        raise RuntimeError(f"Unrecognised CRS: {crs}")
-    auth_name = crs.GetAuthorityName(None)
-    auth_code = crs.GetAuthorityCode(None)
+    if authority is not None:
+        auth_name, auth_code = authority
+    else:
+        auth_name, auth_code = parse_authority(crs)
+
     if auth_name and auth_code:
         return f"{auth_name}:{auth_code}"
     code = auth_name or auth_code
     if code and code.strip() not in ("0", "EPSG"):
         return code
-    return f"CUSTOM:{get_identifier_int(crs)}"
+    return f"CUSTOM:{get_identifier_int(crs, (auth_name, auth_code))}"
 
 
-def get_identifier_int(crs):
+def get_identifier_int(crs, authority=None):
     """
     Given a CRS, generate a stable, unique identifer for it of type 'int'. Eg: 2193
     """
-    if isinstance(crs, str):
-        crs = SpatialReference(crs)
-    if not isinstance(crs, SpatialReference):
-        raise RuntimeError(f"Unrecognised CRS: {crs}")
-    auth_code = crs.GetAuthorityCode(None)
+    if authority is not None:
+        auth_name, auth_code = authority
+    else:
+        auth_name, auth_code = parse_authority(crs)
+
     if auth_code and auth_code.isdigit() and int(auth_code) > 0:
         return int(auth_code)
+
+    if isinstance(crs, str):
+        wkt = crs.strip()
+    elif isinstance(crs, SpatialReference):
+        wkt = crs.ExportToPrettyWkt()
+    else:
+        raise RuntimeError(f"Unrecognised CRS: {crs}")
+
     # Stable code that fits easily in an int32 and won't collide with EPSG codes.
-    return (uint32hash(crs.ExportToWkt()) & 0xFFFFFFF) + 1000000
+    return (uint32hash(wkt) & 0xFFFFFFF) + 1000000
 
 
 def get_identifier_int_from_dataset(dataset, crs_name=None):
---
+++ tests/test_crs_util.py
@@ -0,0 +1,32 @@
+from osgeo.osr import SpatialReference
+
+from sno import crs_util
+
+# This is EPSG:4326 but with explicit axes in the order EAST, NORTH.
+# It exists in the wild and it doesn't work well with SpatialReference.
+# We just want to parse it without losing parts of it.
+TEST_WKT = """
+GEOGCS["WGS 84",
+    DATUM["WGS_1984",
+        SPHEROID["WGS 84", 6378137, 298.257223563,
+            AUTHORITY["EPSG", "7030"]],
+        AUTHORITY["EPSG", "6326"]],
+    PRIMEM["Greenwich", 0,
+        AUTHORITY["EPSG", "8901"]],
+    UNIT["degree", 0.0174532925199433,
+        AUTHORITY["EPSG", "9122"]],
+    AXIS["Longitude", EAST],
+    AXIS["Latitude", NORTH],
+    AUTHORITY["EPSG", "4326"]]
+"""
+
+
+def test_parse():
+    assert crs_util.parse_name(TEST_WKT) == "WGS 84"
+    assert crs_util.parse_authority(TEST_WKT) == ("EPSG", "4326")
+
+    # Strangely this doesn't entirely work using osgeo:
+    spatial_ref = SpatialReference(TEST_WKT)
+    assert spatial_ref.GetName() == "WGS 84"
+    assert spatial_ref.GetAuthorityName(None) is None
+    assert spatial_ref.GetAuthorityCode(None) is None
