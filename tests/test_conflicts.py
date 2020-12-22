cule/sno

Turn off find-renames for merge

tests/test_conflicts.py
@@ -3,6 +3,7 @@ import pytest
 
 from sno.merge_util import MergeIndex
 from sno.structs import CommitWithReference
+from sno.sno_repo import SnoRepo
 
 H = pytest.helpers.helpers()
 
@@ -302,3 +303,46 @@ def test_list_conflicts_transform_crs(repo_version, create_conflicts, cli_runner
         assert r.exit_code == 0, r
         coords = json.loads(r.stdout)["features"][0]["geometry"]["coordinates"]
         assert coords == [1975606.0592274915, 5775365.736491613]
+
+
+@pytest.mark.parametrize(*V1_OR_V2)
+def test_find_renames(repo_version, data_working_copy, geopackage, cli_runner):
+    archive = "points2" if repo_version == 2 else "points"
+    with data_working_copy(archive) as (repo_path, wc):
+        db = geopackage(wc)
+
+        cli_runner.invoke(["checkout", "-b", "ancestor_branch"])
+        cli_runner.invoke(["checkout", "-b", "theirs_branch"])
+        with db:
+            db.cursor().execute(
+                f"""UPDATE {H.POINTS.LAYER} SET fid = 9998 where fid = 1"""
+            )
+            assert db.changes() == 1
+
+        cli_runner.invoke(["commit", "-m", "theirs_commit"])
+
+        cli_runner.invoke(["checkout", "ancestor_branch"])
+        cli_runner.invoke(["checkout", "-b", "ours_branch"])
+
+        with db:
+            db.cursor().execute(
+                f"""UPDATE {H.POINTS.LAYER} SET fid = 9999 where fid = 1"""
+            )
+            assert db.changes() == 1
+
+        cli_runner.invoke(["commit", "-m", "ours_commit"])
+
+        repo = SnoRepo(repo_path)
+        ancestor = CommitWithReference.resolve(repo, "ancestor_branch")
+        ours = CommitWithReference.resolve(repo, "ours_branch")
+        theirs = CommitWithReference.resolve(repo, "theirs_branch")
+
+        index = repo.merge_trees(ancestor.tree, ours.tree, theirs.tree)
+
+        assert index.conflicts
+
+        index = repo.merge_trees(
+            ancestor.tree, ours.tree, theirs.tree, flags={"find_renames": False}
+        )
+
+        assert not index.conflicts

koordinates/kart

tests/test_conflicts.py
@@ -2,6 +2,7 @@ import json
 import pytest
 
 from sno.merge_util import MergeIndex
+from sno.repo import SnoRepo
 from sno.structs import CommitWithReference
 
 H = pytest.helpers.helpers()
@@ -301,3 +302,46 @@ def test_list_conflicts_transform_crs(repo_version, create_conflicts, cli_runner
         assert r.exit_code == 0, r
         coords = json.loads(r.stdout)["features"][0]["geometry"]["coordinates"]
         assert coords == [1975606.0592274915, 5775365.736491613]
+
+
+@pytest.mark.parametrize(*V1_OR_V2)
+def test_find_renames(repo_version, data_working_copy, geopackage, cli_runner):
+    archive = "points2" if repo_version == 2 else "points"
+    with data_working_copy(archive) as (repo_path, wc):
+        db = geopackage(wc)
+
+        cli_runner.invoke(["checkout", "-b", "ancestor_branch"])
+        cli_runner.invoke(["checkout", "-b", "theirs_branch"])
+        with db:
+            db.cursor().execute(
+                f"""UPDATE {H.POINTS.LAYER} SET fid = 9998 where fid = 1"""
+            )
+            assert db.changes() == 1
+
+        cli_runner.invoke(["commit", "-m", "theirs_commit"])
+
+        cli_runner.invoke(["checkout", "ancestor_branch"])
+        cli_runner.invoke(["checkout", "-b", "ours_branch"])
+
+        with db:
+            db.cursor().execute(
+                f"""UPDATE {H.POINTS.LAYER} SET fid = 9999 where fid = 1"""
+            )
+            assert db.changes() == 1
+
+        cli_runner.invoke(["commit", "-m", "ours_commit"])
+
+        repo = SnoRepo(repo_path)
+        ancestor = CommitWithReference.resolve(repo, "ancestor_branch")
+        ours = CommitWithReference.resolve(repo, "ours_branch")
+        theirs = CommitWithReference.resolve(repo, "theirs_branch")
+
+        index = repo.merge_trees(ancestor.tree, ours.tree, theirs.tree)
+
+        assert index.conflicts
+
+        index = repo.merge_trees(
+            ancestor.tree, ours.tree, theirs.tree, flags={"find_renames": False}
+        )
+
+        assert not index.conflicts