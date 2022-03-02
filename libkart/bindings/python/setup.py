5e63952b90476af3dbed9e6811246d358d463bea
rcoup <robert.coup@koordinates.com>
2022-03-02 23:22:49 +0000

    cmake: updates
    - split out main libkart CMakeLists into different directories
    - use venv for building python bindings
    - use LibGit2_ROOT if specified for cppgit2, otherwise use vendored
    - use CCache

--- libkart/bindings/python/setup.py
+++ libkart/bindings/python/setup.py
@@ -20,16 +20,9 @@ setup(
             "libkart",
             sources=["libkart.pyx"],
             language="c++",
-            extra_compile_args=["--std=c++17", "-O0", "-g"],
+            extra_compile_args=["--std=c++20", "-O0", "-g"],
             libraries=["kart"],
-            # TODO: how do we move these into whatever script (cmake presumably) is invoking setup.py?
-            library_dirs=[str(LIBKART_BUILD_DIR)],
-            extra_link_args=["-rpath", str(LIBKART_BUILD_DIR)],
-            include_dirs=[
-                "include",
-                str(LIBKART_SRC_PREFIX / "include"),
-                str(LIBKART_SRC_PREFIX / "cppgit2" / "include"),
-            ],
+            # extra_link_args=["-rpath", str(LIBKART_BUILD_DIR)],
         ),
         compiler_directives={"language_level": "3"},
     ),
