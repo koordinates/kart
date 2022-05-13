e5c6b2e0357bc8ae5a19e7889bf29aa08049edf9
Andrew Olsen <andrew.olsen@koordinates.com>
2022-05-13 15:34:35 +1200

    PC import: Fix working_copy.base.Mismatch
    When importing a PC, tabular WC needs updating too, since it needs
    to track the HEAD tree in kart-state table.

--- kart/point_cloud/import_.py
+++ kart/point_cloud/import_.py
@@ -254,6 +254,11 @@ def point_cloud_import(ctx, convert_to_copc, ds_path, sources):
     click.echo("Updating working copy...")
     reset_wc_if_needed(repo)
 
+    # TODO - fix up reset code - there should be a single function you can call that updates all working copies.
+    tabular_wc = repo.get_working_copy(allow_uncreated=True)
+    if tabular_wc is not None:
+        tabular_wc.reset(repo.head_commit)
+
 
 def _unwrap_metadata(metadata):
     if isinstance(metadata, str):
