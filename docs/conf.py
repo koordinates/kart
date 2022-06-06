43090248461593c29fbe2f871015b843e14f3822
Andrew Olsen <andrew.olsen@koordinates.com>
2022-06-07 09:38:27 +1200

    Release v0.11.3

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ sys.path.append(".")
 project = "Kart"
 copyright = "2022, Kart Contributors"
 author = "Kart Contributors"
-release = "0.11.2"
+release = "0.11.3"
 
 
 # -- General configuration ---------------------------------------------------
