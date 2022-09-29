873d42bfc44932e29adce98b5e732019c01e4519
Andrew Olsen <andrew.olsen@koordinates.com>
2022-09-30 11:37:19 +1300

    Set rpath on macos

--- vcpkg-vendor/vcpkg-overlay-triplets/arm64-osx.cmake
+++ vcpkg-vendor/vcpkg-overlay-triplets/arm64-osx.cmake
@@ -7,6 +7,8 @@ set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
 set(VCPKG_OSX_ARCHITECTURES arm64)
 set(VCPKG_OSX_DEPLOYMENT_TARGET "11.0")
 
+set(ENV{LDFLAGS} -Wl,-rpath,${CURRENT_INSTALLED_DIR}/lib/)
+
 # https://github.com/microsoft/vcpkg/issues/10038
 set(VCPKG_C_FLAGS "-mmacosx-version-min=11.0")
 set(VCPKG_CXX_FLAGS "-mmacosx-version-min=11.0")
