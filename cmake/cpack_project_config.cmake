3e20c9ed3f610f33cb2a00022f248205c4077722
rcoup <robert.coup@koordinates.com>
2022-12-13 15:59:37 +0000

    ci: windows: force msi version to x.y.z.

--- cmake/cpack_project_config.cmake
+++ cmake/cpack_project_config.cmake
@@ -1,16 +1,23 @@
-# Exclude component "addToPath" for TGZ & ZIP.
-# Is handled in post-build for RPM & DEB
-if (CPACK_GENERATOR MATCHES "^(TGZ|ZIP|DEB|RPM)$")
+# Exclude component "addToPath" for TGZ & ZIP. Is handled in post-build for RPM & DEB
+if(CPACK_GENERATOR MATCHES "^(TGZ|ZIP|DEB|RPM)$")
   list(REMOVE_ITEM CPACK_COMPONENTS_ALL "addToPath")
 endif()
 
 # Selectively decide whether to include kart-1.2.3/ dir at the top level
-if (CPACK_GENERATOR MATCHES "^(TGZ|ZIP)$")
+if(CPACK_GENERATOR MATCHES "^(TGZ|ZIP)$")
   set(CPACK_INCLUDE_TOPLEVEL_DIRECTORY ON)
   set(CPACK_COMPONENT_INCLUDE_TOPLEVEL_DIRECTORY ON)
 endif()
 
-# Need to set 'CMAKE_<GENERATOR>_COMPONENT_INSTALL' to ON, otherwise CPack ignores CPACK_COMPONENTS_ALL variable
+if(CPACK_GENERATOR MATCHES "^(WIX)$")
+  # Force to X.Y.Z[.W]
+  set(CPACK_PACKAGE_VERSION
+      "${CMAKE_PROJECT_VERSION_MAJOR}.${CMAKE_PROJECT_VERSION_MINOR}.${CMAKE_PROJECT_VERSION_PATCH}"
+  )
+endif()
+
+# Need to set 'CMAKE_<GENERATOR>_COMPONENT_INSTALL' to ON, otherwise CPack ignores
+# CPACK_COMPONENTS_ALL variable
 set(CPACK_ARCHIVE_COMPONENT_INSTALL ON)
 set(CPACK_RPM_COMPONENT_INSTALL ON)
 set(CPACK_DEB_COMPONENT_INSTALL ON)
