296de08c575ef0cb885b9aac7a2eba4c9e58df57
rcoup <robert.coup@koordinates.com>
2022-12-21 22:07:43 +0000

    macos: - fix bug in e2e tests where it was looking for Python2 - make ci python install quieter

--- tests/scripts/e2e-1.sh
+++ tests/scripts/e2e-1.sh
@@ -4,7 +4,7 @@ set -eu
 if ! command -v realpath >/dev/null; then
     # MacOS doesn't have realpath or readlink -f by default
     function realpath() {
-        python -c 'import os, sys; print(os.path.realpath(sys.argv[1]))' "$1"
+        python3 -c 'import os, sys; print(os.path.realpath(sys.argv[1]))' "$1"
     }
 fi
 
