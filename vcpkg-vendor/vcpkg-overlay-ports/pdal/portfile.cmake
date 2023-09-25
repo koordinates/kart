ad39596d513c366ee48811d0c794f305fd6c72c6
Andrew Olsen <andrew.olsen@koordinates.com>
2023-09-25 13:12:43 +1300

    Switch to kx-fork of PDAL 2.5.6 with cherrypicks:
    https://github.com/PDAL/PDAL/pull/4133
    https://github.com/PDAL/PDAL/pull/4180
    https://github.com/PDAL/PDAL/pull/4183
    
    Improved conformance to COPC spec gets rid of some warnings.
    Fix for the COPC writer truncating scale / offset values, which
    could cause point coordinates to shift by small amounts.

---
+++ vcpkg-vendor/vcpkg-overlay-ports/pdal/portfile.cmake
@@ -0,0 +1,120 @@
+vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO koordinates/PDAL
+    REF kart-v0.14.2
+    SHA512 721010c6842a841e8a7d4db543022669ea818e78c87f468d672b1fd77f445897735135508e5b060d17a5b88456eea3a4a66c0c258ac418d995a07ad2804c3fa3
+    HEAD_REF kart-v0.14.2
+    PATCHES
+        fix-dependency.patch
+        fix-unix-compiler-options.patch
+        fix-find-library-suffix.patch
+        no-pkgconfig-requires.patch
+        no-rpath.patch
+)
+
+# Prefer pristine CMake find modules + wrappers and config files from vcpkg.
+foreach(package IN ITEMS Curl GeoTIFF ICONV ZSTD)
+    file(REMOVE "${SOURCE_PATH}/cmake/modules/Find${package}.cmake")
+endforeach()
+
+# De-vendoring
+file(REMOVE_RECURSE
+    "${SOURCE_PATH}/vendor/nanoflann"
+    "${SOURCE_PATH}/vendor/nlohmann"
+    "${SOURCE_PATH}/pdal/JsonFwd.hpp"
+)
+file(INSTALL "${CURRENT_INSTALLED_DIR}/include/nanoflann.hpp" DESTINATION "${SOURCE_PATH}/vendor/nanoflann")
+file(INSTALL "${CURRENT_INSTALLED_DIR}/include/nlohmann/json.hpp" DESTINATION "${SOURCE_PATH}/vendor/nlohmann/nlohmann")
+file(APPEND "${SOURCE_PATH}/vendor/nlohmann/nlohmann/json.hpp" "namespace NL = nlohmann;\n")
+file(INSTALL "${CURRENT_INSTALLED_DIR}/include/nlohmann/json_fwd.hpp" DESTINATION "${SOURCE_PATH}/pdal")
+file(RENAME "${SOURCE_PATH}/pdal/json_fwd.hpp" "${SOURCE_PATH}/pdal/JsonFwd.hpp")
+file(APPEND "${SOURCE_PATH}/pdal/JsonFwd.hpp" "namespace NL = nlohmann;\n")
+
+unset(ENV{OSGEO4W_HOME})
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        draco       BUILD_PLUGIN_DRACO
+        e57         BUILD_PLUGIN_E57
+        hdf5        BUILD_PLUGIN_HDF
+        i3s         BUILD_PLUGIN_I3S
+        lzma        WITH_LZMA
+        pgpointcloud BUILD_PLUGIN_PGPOINTCLOUD
+        zstd        WITH_ZSTD
+)
+vcpkg_find_acquire_program(PKGCONFIG)
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        "-DCMAKE_PROJECT_INCLUDE=${CMAKE_CURRENT_LIST_DIR}/cmake-project-include.cmake"
+        -DPDAL_PLUGIN_INSTALL_PATH=.
+        "-DPKG_CONFIG_EXECUTABLE=${PKGCONFIG}"
+        -DWITH_TESTS:BOOL=OFF
+        -DWITH_COMPLETION:BOOL=OFF
+        -DCMAKE_DISABLE_FIND_PACKAGE_Libexecinfo:BOOL=ON
+        -DCMAKE_DISABLE_FIND_PACKAGE_Libunwind:BOOL=ON
+        ${FEATURE_OPTIONS}
+)
+
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/PDAL)
+vcpkg_fixup_pkgconfig()
+vcpkg_copy_pdbs()
+
+# Install and cleanup executables
+file(GLOB pdal_unsupported
+    "${CURRENT_PACKAGES_DIR}/bin/*.bat"
+    "${CURRENT_PACKAGES_DIR}/bin/pdal-config"
+    "${CURRENT_PACKAGES_DIR}/debug/bin/*.bat"
+    "${CURRENT_PACKAGES_DIR}/debug/bin/*.exe"
+    "${CURRENT_PACKAGES_DIR}/debug/bin/pdal-config"
+)
+file(REMOVE ${pdal_unsupported})
+vcpkg_copy_tools(TOOL_NAMES pdal AUTO_CLEAN)
+
+# Post-install clean-up
+file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/include/pdal/filters/private/csf"
+    "${CURRENT_PACKAGES_DIR}/include/pdal/filters/private/miniball"
+    "${CURRENT_PACKAGES_DIR}/debug/include"
+    "${CURRENT_PACKAGES_DIR}/debug/share"
+)
+
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+
+file(READ "${SOURCE_PATH}/LICENSE.txt" pdal_license)
+file(READ "${SOURCE_PATH}/vendor/arbiter/LICENSE" arbiter_license)
+file(READ "${SOURCE_PATH}/vendor/kazhdan/PoissonRecon.h" kazhdan_license)
+string(REGEX REPLACE "^/\\*\n|\\*/.*\$" "" kazhdan_license "${kazhdan_license}")
+file(READ "${SOURCE_PATH}/vendor/lazperf/lazperf.hpp" lazperf_license)
+string(REGEX REPLACE "^/\\*\n|\\*/.*\$" "" lazperf_license "${lazperf_license}")
+file(WRITE "${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright"
+"${pdal_license}
+---
+
+Files in vendor/arbiter/:
+
+${arbiter_license}
+---
+
+Files in vendor/kazhdan/:
+
+${kazhdan_license}
+---
+
+Files in vendor/lazperf/:
+
+${lazperf_license}
+---
+
+Files in vendor/eigen:
+
+Most Eigen source code is subject to the terms of the Mozilla Public License
+v. 2.0. You can obtain a copy the MPL 2.0 at http://mozilla.org/MPL/2.0/.
+
+Some files included in Eigen are under one of the following licenses:
+ - Apache License, Version 2.0 
+ - BSD 3-Clause \"New\" or \"Revised\" License
+")
