95471bfc2550a339090f0bdc410dfb36dc891369
Andrew Olsen <andrew.olsen@koordinates.com>
2023-10-02 10:29:52 +1300

    Fix broken documentation link

--- kart/data.py
+++ kart/data.py
@@ -163,7 +163,7 @@ def data_version(ctx, output_format):
         click.echo(f"This Kart repo uses Datasets v{version}")
         if version >= 1:
             click.echo(
-                f"(See https://docs.kartproject.org/en/latest/pages/development/datasets_v{version}.html#datasets-v{version})"
+                f"(See https://docs.kartproject.org/en/latest/pages/development/table_v{version}.html"
             )
     elif output_format == "json":
         from .repo import KartConfigKeys
