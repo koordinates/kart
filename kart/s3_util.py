67550265de4ba88a6ee6e78c032bad2be8fe63f1
Andrew Olsen <andrew.olsen@koordinates.com>
2023-10-05 16:30:47 +1300

    Improved configuration / resource reuse for S3
    Learn the region that buckets are in, where possible
    Configure S3 the client to default to the region that the resource is in,
    to save it potentially finding the region itself on a per-request basis
    Dont' reuse clients across multiple regions (for the config reason)
    Don't reuse clients across multiple threads (S3 resources and sessions
    are not guaranteed to be thread safe)
    Do reuse clients / resources / buckets in other cases -
    these will be cached until the command exits
    
    This is the also the groundwork for fetching tiles during WC checkout,
    which will probably need to happen on multiple threads.

--- kart/s3_util.py
+++ kart/s3_util.py
@@ -1,8 +1,10 @@
 from base64 import standard_b64decode
+import logging
 import functools
 import os
 from pathlib import Path
 import tempfile
+from threading import currentThread
 from urllib.parse import urlparse
 
 import boto3
@@ -12,32 +14,82 @@ from kart.exceptions import NotFound, NO_IMPORT_SOURCE, NO_CHECKSUM
 
 # Utility functions for dealing with S3 - not yet launched.
 
+# Lots of functions in this file look like this:
+# >> @functools.lru_cache()
+# >> def _get_some_thread_unsafe_s3_resource(region, thread_hash):
+# This is a simple way of effectively getting a thread-local cache, ie,
+# - we want to get a resource that is configured how we currently need it (has the correct default region)
+# - we don't want a resource that is also being used by another thread
+# - aside from these two constraints, we can reuse the resources, hence the lru_cache.
 
-@functools.lru_cache(maxsize=1)
-def get_s3_config():
-    # TODO - add an option --s3-region to commands where it would be useful.
-    return None
+L = logging.getLogger("kart.s3_util")
 
 
-@functools.lru_cache(maxsize=1)
-def get_s3_client():
-    client = boto3.client("s3", config=get_s3_config())
+@functools.lru_cache()
+def get_region(bucket):
+    # The region -> bucket cache is not thread-local, since the region a bucket is in doesn't depend
+    # on which thread we are currently using.
+    if not bucket:
+        return None
+    try:
+        response = get_s3_client().head_bucket(Bucket=bucket)
+        return response["ResponseMetadata"]["HTTPHeaders"]["x-amz-bucket-region"]
+    except Exception as e:
+        L.warning("Couldn't find S3 region for bucket %s: %s", bucket, e)
+        # We don't necessarily need to know which region a bucket is in -
+        # - we try to configure S3 clients to default to connecting to the right region, for efficiency
+        # - but even if this doesn't work, the overall operation might still work. We'll keep going.
+        return None
+
+
+def get_s3_session(*, region=None, bucket=None):
+    return _get_s3_session(region or get_region(bucket), hash(currentThread()))
+
+
+@functools.lru_cache()
+def _get_s3_session(region, thread_hash):
+    return boto3.session.Session(region_name=region)
+
+
+def get_s3_client(*, region=None, bucket=None):
+    return _get_s3_client(region or get_region(bucket), hash(currentThread()))
+
+
+@functools.lru_cache()
+def _get_s3_client(region, thread_hash):
+    client = _get_s3_session(region, thread_hash).client("s3")
     if "AWS_NO_SIGN_REQUEST" in os.environ:
         client._request_signer.sign = lambda *args, **kwargs: None
     return client
 
 
-@functools.lru_cache(maxsize=1)
-def get_s3_resource():
-    resource = boto3.resource("s3", config=get_s3_config())
+def get_s3_resource(*, region=None, bucket=None):
+    return _get_s3_resource(region or get_region(bucket), hash(currentThread()))
+
+
+@functools.lru_cache()
+def _get_s3_resource(region, thread_hash):
+    resource = _get_s3_session(region, thread_hash).resource("s3")
     if "AWS_NO_SIGN_REQUEST" in os.environ:
         resource.meta.client._request_signer.sign = lambda *args, **kwargs: None
     return resource
 
 
+def get_s3_bucket(bucket):
+    return _get_s3_bucket(bucket, hash(currentThread()))
+
+
 @functools.lru_cache()
-def get_bucket(name):
-    return get_s3_resource().Bucket(name)
+def _get_s3_bucket(bucket, thread_hash):
+    return get_s3_resource(bucket=bucket).Bucket(bucket)
+
+
+def parse_s3_url(s3_url):
+    parsed = urlparse(s3_url)
+    assert parsed.scheme == "s3"
+    bucket = parsed.netloc
+    key = parsed.path.lstrip("/")
+    return bucket, key
 
 
 def fetch_from_s3(s3_url, output_path=None):
@@ -46,14 +98,13 @@ def fetch_from_s3(s3_url, output_path=None):
     If output-path is not set, creates a temporary file using tempfile.mkstemp()
     """
     # TODO: handle failure.
-    parsed = urlparse(s3_url)
-    bucket = get_bucket(parsed.netloc)
+    bucket, key = parse_s3_url(s3_url)
     if output_path is None:
-        fd, path = tempfile.mkstemp()
+        fd, output_path = tempfile.mkstemp()
         # If we keep it open, boto3 won't be able to write to it (on Windows):
         os.close(fd)
-        output_path = Path(path)
-    bucket.download_file(parsed.path.lstrip("/"), str(output_path.resolve()))
+    output_path = Path(output_path).resolve()
+    get_s3_bucket(bucket).download_file(key, str(output_path))
     return output_path
 
 
@@ -67,8 +118,15 @@ def expand_s3_glob(source_spec):
     if "*" not in source_spec:
         return [source_spec]
 
-    parsed = urlparse(source_spec)
-    prefix, suffix = parsed.path.split("*", maxsplit=1)
+    bucket, key = parse_s3_url(source_spec)
+    if "*" in bucket:
+        raise click.UsageError(
+            "Wildcard '*' should only be in key part of s3 URL, not in bucket"
+        )
+    if "*" not in key:
+        return [source_spec]
+
+    prefix, suffix = key.split("*", maxsplit=1)
     if "*" in suffix:
         raise click.UsageError(
             f"Two wildcards '*' found in {source_spec} - only one wildcard is supported"
@@ -76,8 +134,7 @@ def expand_s3_glob(source_spec):
     prefix = prefix.lstrip("/")
     prefix_len = len(prefix)
 
-    bucket = get_bucket(parsed.netloc)
-    matches = bucket.objects.filter(Prefix=prefix)
+    matches = get_s3_bucket(bucket).objects.filter(Prefix=prefix)
     result = []
     for match in matches:
         assert match.key.startswith(prefix)
@@ -94,10 +151,8 @@ def expand_s3_glob(source_spec):
 
 def get_hash_and_size_of_s3_object(s3_url):
     """Returns the (SHA256-hash-in-Base64, filesize) of an S3 object."""
-    parsed = urlparse(s3_url)
-    bucket = parsed.netloc
-    key = parsed.path.lstrip("/")
-    response = get_s3_client().head_object(
+    bucket, key = parse_s3_url(s3_url)
+    response = get_s3_client(bucket=bucket).head_object(
         Bucket=bucket, Key=key, ChecksumMode="ENABLED"
     )
     # TODO: fall back to other ways of learning the checksum.
