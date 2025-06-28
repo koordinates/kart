8a33b9ac470fcf3bc7b38c2e165618b46daa4f0e
Craig de Stigter <craig@destigter.nz>
2025-06-28 22:18:09 +1200

    Bump MacOS deployment target (13+)
    10/11 is really ancient at this point. Macs from 2017-2019 can upgrade
    to macOS 13+, so it's not reasonable to support older.

--- vcpkg-vendor/vcpkg-overlay-triplets/x64-osx.cmake
+++ vcpkg-vendor/vcpkg-overlay-triplets/x64-osx.cmake
@@ -5,11 +5,13 @@ set(VCPKG_BUILD_TYPE release)
 
 set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
 set(VCPKG_OSX_ARCHITECTURES x86_64)
-set(VCPKG_OSX_DEPLOYMENT_TARGET "11.0" CACHE STRING "" FORCE)
+set(VCPKG_OSX_DEPLOYMENT_TARGET
+    "13.0"
+    CACHE STRING "" FORCE)
 
 set(ENV{LDFLAGS} -Wl,-rpath,${CURRENT_INSTALLED_DIR}/lib/)
 
 # https://github.com/microsoft/vcpkg/issues/10038
-set(VCPKG_C_FLAGS "-mmacosx-version-min=11.0")
-set(VCPKG_CXX_FLAGS "-mmacosx-version-min=11.0")
-set(ENV{MACOSX_DEPLOYMENT_TARGET} "11.0")
+set(VCPKG_C_FLAGS "-mmacosx-version-min=13.0")
+set(VCPKG_CXX_FLAGS "-mmacosx-version-min=13.0")
+set(ENV{MACOSX_DEPLOYMENT_TARGET} "13.0")
