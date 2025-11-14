fccaef3bd65a3811b190202a4ac81862afc43e00
Craig de Stigter <craig@destigter.nz>
2025-11-14 14:07:46 +1300

    Release v0.17.0rc3

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ sys.path.append(".")
 project = "Kart"
 copyright = "2022-, Kart Contributors"
 author = "Kart Contributors"
-release = "0.17.0rc2"
+release = "0.17.0rc3"
 
 
 # -- General configuration ---------------------------------------------------
