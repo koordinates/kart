0828b5683ee17cee712660206ae80dffad663aa5
Blayne Chard <blayne@chard.com>
2026-05-08 11:55:04 +1200

    refactor: fix formatting

--- kart/repo.py
+++ kart/repo.py
@@ -554,8 +554,13 @@ class KartRepo(pygit2.Repository):
 
     def validate_gitdir_name(self):
         if not self.is_bare and self.gitdir_path.stem not in self.KART_DIR_NAMES:
-            if self.gitdir_path.stem == ".git" and os.environ.get("KART_ALLOW_FROM_GIT") == "1":
-                L.warning("Selected repo does't follow Kart conventions, but proceeding anyway.")
+            if (
+                self.gitdir_path.stem == ".git"
+                and os.environ.get("KART_ALLOW_FROM_GIT") == "1"
+            ):
+                L.warning(
+                    "Selected repo does't follow Kart conventions, but proceeding anyway."
+                )
                 return
             raise NotFound(
                 "Selected repo doesn't follow Kart convention of keeping internals in a '.kart' folder. Perhaps a git repo?",
